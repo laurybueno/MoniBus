2d0fa63233192282bd78894a79cafcaaf31d3412
Laury Bueno <laury.bueno@gmail.com>
2017-06-20 21:51:19 -0300

    [Registro] Filtro por timestamp adicionado

--- cronotacografo/views.py
+++ cronotacografo/views.py
@@ -1,27 +1,22 @@
-import django_filters
-from django.db import models
-from rest_framework import viewsets, filters
+from rest_framework import viewsets
 from .models import Registro
 from .serializers import RegistroSerializer
-
-
-class TimeFilter(filters.FilterSet):
-    class Meta:
-        model = Registro
-        fields = {
-            'data': ('lte', 'gte'),
-            'p': (),
-        }
-
-    filter_overrides = {
-        models.DateTimeField: {
-            'filter_class': django_filters.IsoDateTimeFilter
-        },
-    }
 
 
 class RegistroViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Registro.objects.all()
     serializer_class = RegistroSerializer
     filter_fields = ('id', 'p', 'a', 'data')
-    # filter_class = TimeFilter
+
+    def get_queryset(self):
+        queryset = Registro.objects.all()
+        # import ipdb; ipdb.set_trace()
+        data_gte = self.request.query_params.get('data_gte', None)
+        if data_gte:
+            queryset = queryset.filter(data__gte=data_gte)
+
+        data_lte = self.request.query_params.get('data_lte', None)
+        if data_lte:
+            queryset = queryset.filter(data__lte=data_lte)
+
+        return queryset
